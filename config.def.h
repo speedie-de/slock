5105d72809ff935f62f7c5aa901dabb1be7c744b
speedie <71722170+speediegq@users.noreply.github.com>
2022-06-02 23:24:32 +0000

    Add files via upload

---
+++ config.def.h
@@ -0,0 +1,31 @@
+/* user and group to drop privileges to */
+static const char *user  = "speedie";
+static const char *group = "video";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] = "black",       /* after initialization */
+	[INPUT] = "#005577",    /* during input */
+	[FAILED] = "#CC3333",   /* wrong password */
+	[CAPS] = "red",         /* CapsLock on */
+};
+
+static const char * message = "";
+static const char * text_color = "#ffffff";
+static const char * font_name  = "6x13";
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+		{ "color0",       STRING,  &colorname[INIT] },
+		{ "color4",       STRING,  &colorname[INPUT] },
+		{ "color1",       STRING,  &colorname[FAILED] },
+		{ "color3",       STRING,  &colorname[CAPS] },
+		{ "message",      STRING,  &message },
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/* Background image path, should be available to the user above */
+static const char* background_image = "/home/speedie/.config/swal/CurrentWallpaper";
